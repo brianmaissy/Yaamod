e250bf29241b2dbb19e291e2bd2060d2e231a118
Greenfeld <yonatangreenfeld97@gmail.com>
2019-10-26 19:53:00 +0300

    Add redirect

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import NavBar from './components/NavBar'
 import LoginForm from './components/Login'
-import Container from '@material-ui/core/Container';
 
 import {
   BrowserRouter as Router,
   Switch,
-  Route
+  Route,
+  Redirect,
+  useHistory
 } from 'react-router-dom';
 import About from './components/About';
 
 
 class App extends React.Component {
+
   render() {
     return (
     <Router>
       <NavBar />
       <Switch>
-        <Route exact path='/' component={LoginForm} />
+        <Route exact path='/manage' component={LoginForm} />
         <Route path='/about' component={About} />
       </Switch>
     </Router>
