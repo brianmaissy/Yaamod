4d6cfebf993f21f32ae66acfaab2b7d8feed3c75
Shachar Buda <shacharbuda27@gmail.com>
2020-02-28 15:55:14 +0200

    api service

---
+++ frontend/src/services/api.service.js
@@ -0,0 +1,30 @@
+import { PERSON_URL } from '../consts/server_routes';
+import Axios from 'axios';
+
+const defaultOnError = (err) => {
+	console.error('err ? ', err);
+}
+
+const METHODS = {
+	GET: 'GET',
+	PATCH: 'PATCH',
+	DELETE: 'DELETE',
+	POST: 'POST'
+}
+
+const apiCall = async (url, method=METHODS.GET, data=undefined) => {
+	try {
+		const axiosRes = await Axios({
+			method,
+			url,
+			data,
+			// crossDomain: true,
+			withCredentials: true
+		});
+		return axiosRes.data;
+	} catch(e) {
+		defaultOnError(e);
+	}
+}
+
+export const getPersonsInSynagouge = async () => await apiCall(PERSON_URL);
