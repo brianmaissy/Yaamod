76ced45581a9ac471f5b667c24938af4446f1dcf
Shachar Buda <shacharbuda27@gmail.com>
2020-03-18 12:44:03 +0200

    get actual data!

--- frontend/src/components/MembersTable.jsx
+++ frontend/src/components/MembersTable.jsx
@@ -1,35 +1,7 @@
 import React from 'react';
 import { Table, TableRow, TableCell, TableHead, TableBody, CircularProgress } from '@material-ui/core'
 import MemberRow from './MemberRow';
-import _ from 'lodash';
 import { getPersonsInSynagouge } from '../services/api.service';
-
-const mockData = [
-		{
-			id: 1,
-			"first_name": "Reuven",
-			"last_name": "Levi",
-			"gender_name": "Male",
-			"paternal_name": "Reuven בן Dad",
-			"maternal_name": "Reuven בן Mom",
-			"yichus_name": "Levi",
-			"father_json": {
-					"id": 1,
-					"name": "Dad"
-			},
-			"mother_json": {
-					"id": 2,
-					"name": "Mom"
-			},
-			"wife_json": {
-					"id": 7,
-					"name": "Rivkah Levi"
-			},
-			"num_of_children": 1
-	}
-];
-
-const data = _.map(_.range(0, 10), (eln, i) => ({...mockData[0], id: i}));
 
 export default class MembersTable extends React.Component {
 	_isMounted;
@@ -63,7 +35,7 @@ export default class MembersTable extends React.Component {
 	}
 
 	render() {
-		const { isLoading } = this.state;
+		const { isLoading, data } = this.state;
 
 		if (isLoading) {
 			return (
