f1e37db9a5ba27644cdbe1b7a24d2b0f8c681dba
Shachar Buda <shacharbuda27@gmail.com>
2020-03-23 12:15:31 +0200

    more spaces

--- frontend/src/services/api.service.js
+++ frontend/src/services/api.service.js
@@ -2,28 +2,28 @@ import { PERSON_URL } from '../consts/server_routes';
 import Axios from 'axios';
 
 const defaultOnError = (err) => {
-	console.error('err ? ', err);
+    console.error('err ? ', err);
 }
 
 const METHODS = {
-	GET: 'GET',
-	PATCH: 'PATCH',
-	DELETE: 'DELETE',
-	POST: 'POST'
+    GET: 'GET',
+    PATCH: 'PATCH',
+    DELETE: 'DELETE',
+    POST: 'POST'
 }
 
 const apiCall = async (url, method=METHODS.GET, data=undefined) => {
-	try {
-		const axiosRes = await Axios({
-			method,
-			url,
-			data,
-			withCredentials: true
-		});
-		return axiosRes.data;
-	} catch(e) {
-		defaultOnError(e);
-	}
+    try {
+        const axiosRes = await Axios({
+            method,
+            url,
+            data,
+            withCredentials: true
+        });
+        return axiosRes.data;
+    } catch(e) {
+        defaultOnError(e);
+    }
 }
 
 export const getPersonsInSynagouge = async () => await apiCall(PERSON_URL);
